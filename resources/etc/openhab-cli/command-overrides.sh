64dcd88a102f2eb36be49a95b4d69a16ad0abed6
Ben Clark <ben@benjyc.uk>
2017-12-16 00:50:28 +0000

    Add method for overriding some cli commands
    Signed-off-by: Ben Clark <ben@benjyc.uk>

---
+++ resources/etc/openhab-cli/command-overrides.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+# For modified behavior of the openhab-cli commands.
+# Uncomment and define the following functions to use them instead of the default.
+
+#showlogs_override(){
+#  #Do something
+#}
+
+#console_override(){
+#  #Do something
+#}
