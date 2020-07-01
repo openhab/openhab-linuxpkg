ba0676345571eec87e8ab4d29286a7aa2dd570e3
Ben Clark <ben@benjyc.uk>
2020-07-01 17:27:42 +0100

    Prepare initial openHAB 3.x packages (#169)
    * Prepare initial openHAB 3.x packages
    * Respond to PR comments
    * Reset variables on install
    * Fix openhab2 being default path on restart
    
    Signed-off-by: Ben Clark <ben@benjyc.uk>

--- resources/etc/profile.d/openhab.sh
+++ resources/etc/profile.d/openhab.sh
@@ -1,20 +1,20 @@
 #!/bin/sh
 
 ## This file sets the environment variables for any user.
-## If you need to edit these, edit the file /etc/default/openhab2.
+## If you need to edit these, edit the file /etc/default/openhab.
 
-if [ -r /etc/default/openhab2 ]; then
-  . /etc/default/openhab2
+if [ -r /etc/default/openhab ]; then
+  . /etc/default/openhab
 fi
 
 if [ -z "${OPENHAB_HTTP_PORT}" ];  then OPENHAB_HTTP_PORT=8080; fi
 if [ -z "${OPENHAB_HTTPS_PORT}" ]; then OPENHAB_HTTPS_PORT=8443; fi
-if [ -z "${OPENHAB_HOME}" ];       then OPENHAB_HOME="/usr/share/openhab2"; fi
-if [ -z "${OPENHAB_CONF}" ];       then OPENHAB_CONF="/etc/openhab2"; fi
+if [ -z "${OPENHAB_HOME}" ];       then OPENHAB_HOME="/usr/share/openhab"; fi
+if [ -z "${OPENHAB_CONF}" ];       then OPENHAB_CONF="/etc/openhab"; fi
 if [ -z "${OPENHAB_RUNTIME}" ];    then OPENHAB_RUNTIME="${OPENHAB_HOME}/runtime"; fi
-if [ -z "${OPENHAB_USERDATA}" ];   then OPENHAB_USERDATA="/var/lib/openhab2"; fi
+if [ -z "${OPENHAB_USERDATA}" ];   then OPENHAB_USERDATA="/var/lib/openhab"; fi
 if [ -z "${OPENHAB_BACKUPS}" ];    then OPENHAB_BACKUPS="${OPENHAB_USERDATA}/backups"; fi
-if [ -z "${OPENHAB_LOGDIR}" ];     then OPENHAB_LOGDIR="/var/log/openhab2"; fi
+if [ -z "${OPENHAB_LOGDIR}" ];     then OPENHAB_LOGDIR="/var/log/openhab"; fi
 if [ -z "${OPENHAB_USER}" ];       then OPENHAB_USER="openhab"; fi
 if [ -z "${OPENHAB_GROUP}" ];      then OPENHAB_GROUP="openhab"; fi
 
